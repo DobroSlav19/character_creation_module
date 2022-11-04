0f6bb90fb40d71cdb1bce4ff7d15e245a24d4072
Вячеслав Абусев <Abdullahabusev@gmail.com>
2022-11-04 13:44:39 +0300

    code according to PEP8,MYPY,PEP257

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+"""Игра где выбирается герой и описываются типы атак."""
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str):
+    """Описание действия attack."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +17,7 @@ def attack(char_name: str, char_class: str):
 
 
 def defence(char_name: str, char_class: str):
+    """Описание действия defence."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -24,6 +27,7 @@ def defence(char_name: str, char_class: str):
 
 
 def special(char_name: str, char_class: str):
+    """описание действия special."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -34,6 +38,7 @@ def special(char_name: str, char_class: str):
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка управления навыками."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +63,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """определение класса игрока."""
     approve_choice = None
     char_class: str
     while approve_choice != 'y':
@@ -80,6 +86,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    """Выбор имени и типа героя."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
